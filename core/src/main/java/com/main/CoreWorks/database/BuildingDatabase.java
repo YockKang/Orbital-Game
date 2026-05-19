e7a7c7400fba542d63be9a12c9a8405e71cd93f5
JY-Yeo <e1511195@u.nus.edu>
2026-05-19 15:14:51 +0800

    Added BuildingDatabase skeleton class

---
+++ core/src/main/java/com/main/CoreWorks/database/BuildingDatabase.java
@@ -0,0 +1,10 @@
+package com.main.CoreWorks.database;
+
+import com.main.CoreWorks.Buildings.Building;
+
+public class BuildingDatabase {
+
+    public static Building createMiner() {
+        return null;
+    }
+}

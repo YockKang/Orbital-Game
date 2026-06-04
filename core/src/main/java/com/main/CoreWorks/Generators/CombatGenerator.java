1b7c192c53594c203400f7c6ed5e948488f1acc6
JY-Yeo <e1511195@u.nus.edu>
2026-06-04 16:27:10 +0800

    Created Generator package for all generation purposes

---
+++ core/src/main/java/com/main/CoreWorks/Generators/CombatGenerator.java
@@ -0,0 +1,13 @@
+package com.main.CoreWorks.Generators;
+
+import com.badlogic.gdx.utils.Array;
+import com.main.CoreWorks.entities.Enemy;
+
+import java.util.Random;
+
+public class CombatGenerator {
+    public static Array<Enemy> createCombat(int depth, Random random) {
+        Array<Enemy> enemies = new Array<>();
+    }
+
+}

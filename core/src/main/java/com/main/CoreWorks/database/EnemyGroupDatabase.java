9dacae216b8a783c4a2f4e9778f6da7b4b500518
YK <yockkang@gmail.com>
2026-06-18 15:36:45 +0800

    Reconverted EnemyGroupDatabase to key by string

--- core/src/main/java/com/main/CoreWorks/database/EnemyGroupDatabase.java
+++ core/src/main/java/com/main/CoreWorks/database/EnemyGroupDatabase.java
@@ -6,14 +6,14 @@ import com.main.CoreWorks.entities.*;
 import java.util.Random;
 
 public class EnemyGroupDatabase {
-    private static final ObjectMap<Integer, Array<Array<EnemyFactory>>> EnemyGroupDB = new ObjectMap<>(3);
+    private static final ObjectMap<String, Array<Array<EnemyFactory>>> EnemyGroupDB = new ObjectMap<>(3);
 
     public static Array<Enemy> register(JsonValue data) {
         if (data.isArray()) {
             data.forEach(com.main.CoreWorks.database.EnemyGroupDatabase::register);
             return null;
         } else {
-            int tier = data.getInt("Tier") - 1;
+            String tier = data.getString("Tier");
             String[] enemies = data.get("Enemies").asStringArray();
             Array<EnemyFactory> arr = new Array<>();
             for (String e : enemies) {
@@ -29,19 +29,31 @@ public class EnemyGroupDatabase {
         }
     }
 
-    public static Array<EnemyFactory> getGroupConstructor(int tier, int num) {
-        return EnemyGroupDB.get(tier - 1).get(num);
+    public static Array<EnemyFactory> getGroupConstructor(String tier, int num) {
+        return EnemyGroupDB.get(tier).get(num);
     }
 
-    public static Array<Enemy> getGroup(int tier, int num, float multiplier) {
+    public static Array<EnemyFactory> getGroupConstructor(int tier, int num) {
+        return getGroupConstructor(String.valueOf(tier), num);
+    }
+
+    public static Array<Enemy> getGroup(String tier, int num, float multiplier) {
         Array<Enemy> arr = new Array<>();
-        EnemyGroupDB.get(tier - 1).get(num).forEach(enemyFactory -> arr.add(enemyFactory.of(multiplier)));
+        EnemyGroupDB.get(tier).get(num).forEach(enemyFactory -> arr.add(enemyFactory.of(multiplier)));
         return arr;
     }
 
+    public static Array<Enemy> getGroup(int tier, int num, float multiplier) {
+        return getGroup(String.valueOf(tier), num, multiplier);
+    }
+
     public static Array<Enemy> getRandomGroup(int tier, float multiplier, Random random) {
+        return getRandomGroup(String.valueOf(tier), multiplier, random);
+    }
+
+    public static Array<Enemy> getRandomGroup(String tier, float multiplier, Random random) {
         Array<Enemy> arr = new Array<>();
-        Array<Array<EnemyFactory>> group = EnemyGroupDB.get(tier - 1);
+        Array<Array<EnemyFactory>> group = EnemyGroupDB.get(tier);
         group.get(random.nextInt(group.size)).forEach(enemyFactory -> arr.add(enemyFactory.of(multiplier)));
         return arr;
     }

d63f447fb3cba266b791c8dca52a89859cdc830f
YK <yockkang@gmail.com>
2026-06-18 15:39:19 +0800

    changed fixedupgrate to not extend upgrade

--- core/src/main/java/com/main/CoreWorks/Rewards/AddFixedUpgradeReward.java
+++ core/src/main/java/com/main/CoreWorks/Rewards/AddFixedUpgradeReward.java
@@ -4,11 +4,12 @@ import com.main.CoreWorks.Factory.Building;
 import com.main.CoreWorks.Factory.Upgrade.Upgrade;
 import com.main.CoreWorks.RunPersistence.RunState;
 
-public class AddFixedUpgradeReward extends AddUpgradeReward{
+public class AddFixedUpgradeReward extends Reward{
+    protected Upgrade upgrade;
     protected Building building;
 
     public AddFixedUpgradeReward(Building bldg, Upgrade upg) {
-        super(String.format("Give %s %s", bldg.displayName(), upg.display()), upg);
+        super("Upgrade a building", String.format("Give %s %s", bldg.displayName(), upg.display()));
         this.building = bldg;
         this.upgrade = upg;
     }
@@ -16,5 +17,10 @@ public class AddFixedUpgradeReward extends AddUpgradeReward{
     @Override
     public void apply(RunState runState) {
         upgrade.execute(building);
+    }
+
+    @Override
+    public boolean needTarget() {
+        return true;
     }
 }
